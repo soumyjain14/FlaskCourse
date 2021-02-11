7d5fcd7807e3e657e6fb50daaf0074c94155e5c6
Soumy Jain <soumyjain14@gmail.com>
2021-02-11 16:41:45 +0530

    Hardwork pays off with Flask and model hosted on web

--- Flask with ML/app1.py
+++ Flask with ML/app1.py
@@ -1,32 +1,25 @@
 from flask import Flask, render_template, redirect, request
+import joblib
+
+model=joblib.load("model.pkl")
 
 app1=Flask(__name__)
 
-friends=['Ram','Shyam','Dham']
-num=5
-
 @app1.route('/')
 def hello():
-	return render_template("index.html",my_friends=friends,number=num)
+	return render_template("index.html")
 
-@app1.route('/about')
-def about():
-	return "<h1> About Page </h1>"
-
-@app1.route('/home',)
-def home():
-	return redirect ("/")
-
-@app1.route('/submit',methods=['POST'])
-def submit():
+@app1.route('/',methods=['POST'])
+def marks():
 	if request.method == 'POST':
-		name=request.form['Your name']
+		hours=float(request.form['hours'])
+		marks=str(model.predict([[hours]]))
+		marks=marks[2:-2]
 
-		f=request.files['userfiles']
-		print(f)
-		f.save(f.filename)
 
-	return "<h1> Hello {}".format(name)
+	#return "<h1> Your marks are: {}".format(marks)-one method
+	return render_template("index.html",your_marks=marks)
+	
 
 
 
