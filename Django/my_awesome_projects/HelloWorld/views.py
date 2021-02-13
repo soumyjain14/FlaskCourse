c46a6629c787e18edd66e0f60d2e1ad0a4988b08
Soumy Jain <soumyjain14@gmail.com>
2021-02-13 16:09:57 +0530

    Django working with templates

--- Django/my_awesome_projects/HelloWorld/views.py
+++ Django/my_awesome_projects/HelloWorld/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    response=render(request,'index.html')
+    university_selected="University of Maryland"
+    list_of_all=[
+            "University of Florida",
+            "Arizona State University",
+            "Indiana University",
+            "Virginia Tech",
+            "Colorado Boulder",
+    ]
+    context={
+        "university": university_selected,
+        "list":list_of_all,
+    }
+    response=render(request,'index.html',context)
     return response
     
